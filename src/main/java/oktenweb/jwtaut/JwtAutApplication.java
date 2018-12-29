a276622f2aaf7f3f66459e7a8af6afad47fb04ac
sergeyokten <sergey.okten@gmail.com>
2018-12-29 19:45:36 +0200

    Initial commit

---
+++ src/main/java/oktenweb/jwtaut/JwtAutApplication.java
@@ -0,0 +1,14 @@
+package oktenweb.jwtaut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAutApplication.class, args);
+    }
+
+}
+
